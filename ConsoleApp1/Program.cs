a19d79b71be7a5bf94c8a01101f6ea6c1bccde4f
Aknur Tazhibek <aknurt@mail.ru>
2018-08-31 14:06:20 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Beep();
+            Console.WriteLine("Введите свое имя: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+    }
+}
